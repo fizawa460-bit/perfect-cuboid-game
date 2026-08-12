1057690b2023b97d5a684c15aeb8eed5c37bb78a
fizawa460-bit <fizawa460@gmail.com>
2026-08-13 07:45:02 +0900

    Add Stage15-6bh support audit utility

---
+++ stages/stage15/scripts/stage15_6bh_support_restatement.py
@@ -0,0 +1,6 @@
+def audit_flags():
+    return {
+        'fixed_s_fiber_subpolynomial': True,
+        'support_equivalent_to_n2_up_to_bo1': True,
+        'support_theorem_as_blackbox_circular': True,
+    }
