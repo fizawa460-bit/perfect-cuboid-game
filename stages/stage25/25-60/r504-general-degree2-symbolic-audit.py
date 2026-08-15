dd86d75a8dfa937082870a73191d76a1c4c68590
fizawa460-bit <fizawa460@gmail.com>
2026-08-15 21:08:14 +0900

    Stage25-60: finalize general degree-2 symbolic verifier

--- stages/stage25/25-60/r504-general-degree2-symbolic-audit.py
+++ stages/stage25/25-60/r504-general-degree2-symbolic-audit.py
@@ -4,8 +4,7 @@ import sympy as sp
 a,b,x=sp.symbols('a b x')
 Q=sp.expand((a*x+b)**4+(x+1)**4)
 A=sp.expand(Q.coeff(x,4)); B=sp.expand(Q.coeff(x,3)); C=sp.expand(Q.coeff(x,2)); D=sp.expand(Q.coeff(x,1)); E=sp.expand(Q.coeff(x,0))
-cond=sp.factor(E*B**2-A*D**2)
-assert cond==16*(a-b)**3*(a+b)*(a*b-1)*(a*b+1)
+assert sp.factor(E*B**2-A*D**2)==16*(a-b)**3*(a+b)*(a*b-1)*(a*b+1)
 
 def IJ(A,B,C,D,E):
     I=sp.factor(12*A*E-3*B*D+C**2)
@@ -14,15 +13,13 @@ def IJ(A,B,C,D,E):
 
 subs={b:-a}
 AA,BB,CC,DD,EE=[sp.factor(z.subs(subs)) for z in (A,B,C,D,E)]
-q1=(AA,0,sp.factor(BB-4*AA),0,sp.factor(2*AA-2*BB+CC))
-I1,J1=IJ(*q1)
+I1,J1=IJ(AA,0,sp.factor(BB-4*AA),0,sp.factor(2*AA-2*BB+CC))
 assert I1==64*a**4*(4*a**4+3)
 assert J1==-1024*a**8*(8*a**4+9)
 
 subs={b:1/a}; lam=1/a
 AA,BB,CC,DD,EE=[sp.factor(z.subs(subs)) for z in (A,B,C,D,E)]
-q2=(AA,0,sp.factor(BB-4*lam*AA),0,sp.factor(2*lam**2*AA-2*lam*BB+CC))
-I2,J2=IJ(*q2)
+I2,J2=IJ(AA,0,sp.factor(BB-4*lam*AA),0,sp.factor(2*lam**2*AA-2*lam*BB+CC))
 assert I2==8*(a-1)**4*(5*a**4+4*a**3+6*a**2+4*a+5)/a**2
 assert J2==-64*(a-1)**8*(a**2+a+1)*(7*a**2+10*a+7)/a**3
 
